c3d6e8cc44596f73117b0ada8170711581853106
Jayden Banks <jayden.banks21@gmail.com>
2021-10-26 17:24:57 -0600

    deployment

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('client'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.css'))
+// })
+// app.get('/assets', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../assets/Bumblebee.jpg'))
+// })
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
